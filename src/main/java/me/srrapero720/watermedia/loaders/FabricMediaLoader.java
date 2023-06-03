7c372dd06cd6990200eb8e3d9643b4a9ceca1078
SrRapero720 <SrRapero720@hotmail.com>
2023-06-03 10:48:28 -0600

    more early loading on fabric

--- src/main/java/me/srrapero720/watermedia/loaders/FabricMediaLoader.java
+++ src/main/java/me/srrapero720/watermedia/loaders/FabricMediaLoader.java
@@ -3,24 +3,27 @@ package me.srrapero720.watermedia.loaders;
 import com.mojang.logging.LogUtils;
 import me.srrapero720.watermedia.WaterMedia;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
 import org.slf4j.Logger;
 
 /**
  * Loader for FABRIC
  */
-public class FabricMediaLoader implements ClientModInitializer {
+public class FabricMediaLoader implements ModInitializer {
     private static final Logger LOGGER = LogUtils.getLogger();
     private static void load() {
         WaterMedia.load(FabricLoader.getInstance().getGameDir(), FabricLoader.getInstance().isDevelopmentEnvironment());
     }
 
+    public FabricMediaLoader() {
+        if (FabricLoader.getInstance().getEnvironmentType().equals(EnvType.SERVER)) {
+            if (FabricLoader.getInstance().isDevelopmentEnvironment()) LOGGER.warn("WATERMeDIA shouldn't be installed on server. Ignoring crash because we are in DEV MODE.");
+            else throw new IllegalStateException("WATERMeDIA shouldn't be installed on server!!");
+        } else load();
+    }
 
     @Override
-    public void onInitializeClient() {
-        // Not required
-//        if (FabricLoader.getInstance().getEnvironmentType().equals(EnvType.SERVER))
-//            throw new IllegalStateException("WATERMeDIA shouldn't be installed on server!!");
-        load();
-    }
+    public void onInitialize() {}
 }
