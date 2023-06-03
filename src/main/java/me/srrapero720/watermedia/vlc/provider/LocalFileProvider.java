84714e14e984b173bb06bc5cf458e17c6a45ae89
Goedix <goedix+inazucraft@wearehackerone.com>
2023-06-03 15:33:25 +0200

    feat: Checking binary versions

--- src/main/java/me/srrapero720/watermedia/vlc/provider/LocalFileProvider.java
+++ src/main/java/me/srrapero720/watermedia/vlc/provider/LocalFileProvider.java
@@ -1,6 +1,8 @@
 package me.srrapero720.watermedia.vlc.provider;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 import me.srrapero720.watermedia.vlc.extractor.DLLExtractor;
@@ -21,22 +23,45 @@ public class LocalFileProvider implements DiscoveryDirectoryProvider {
     @Override public String[] directories() {
         var vlc = GAME_DIR.resolve("cache/vlc/");
         LOGGER.info("Path from file extraction is '{}'", vlc.toAbsolutePath().toFile());
+        var config = vlc.resolve("version.cfg");
+        boolean shouldExtract;
 
-        // HAY QUE REVISAR SI YA EXISTEN BINARIOS O SI HAY QUE ACTUALIZARLOS
-        // SOLO DEBE EXTRAER LOS BINARIOS SI HAY QUE ACTUALIZAR LOS BINARIOS (SI EXISTEN ARCHIVOS DEBE BORRARLOS Y EXTRAER)
-        // O SI NO EXISTEN ARCHIVOS.
-        // CASO CONTRARIO NO DEBE HACER NADA
-        // TODO: Crear un archivo de configuracion y que este determine si debe re-extraer los bins
-        if (true) {
+        // Check if we need to update binaries
+        try {
+            if (Files.exists(config)) {
+                String configVersion = Files.readString(config);
+                // Add a way to get the current VLC version you want to install
+                String expectedVersion = getCurrentVlcVersion();
+                shouldExtract = !configVersion.equals(expectedVersion);
+            } else {
+                shouldExtract = true;
+            }
+        } catch (IOException e) {
+            LOGGER.error("Could not read configuration file", e);
+            shouldExtract = true;
+        }
+
+        if (shouldExtract) {
             LOGGER.warn("Extracting BINS before VLCJ attempt to load local files");
             for (var binary: DLLExtractor.values()) binary.extract();
 
             LOGGER.warn("Also extracting LUA scripts");
             for (var luac: LuaExtractor.values()) luac.extract();
+
+            try {
+                // Write the version of VLC binaries you just installed
+                Files.writeString(config, getCurrentVlcVersion());
+            } catch (IOException e) {
+                LOGGER.error("Could not write to configuration file", e);
+            }
         } else {
             LOGGER.warn("Local bins detected, ignoring extractor");
         }
 
         return new String[] { vlc.toAbsolutePath().toString() };
+    }
+
+    private String getCurrentVlcVersion() {
+        return "1.0.0";
     }
 }
