197700cb7079ace886bf006469570d496f73980d
SrRapero720 <SrRapero720@hotmail.com>
2023-06-08 03:45:43 -0600

    VLCJ4: added UnAllocBufferFormatCallback (to skip allocatedBuffers method)

--- src/main/java/me/lib720/caprica/vlcj4/player/embedded/videosurface/callback/UnAllocBufferFormatCallback.java
+++ src/main/java/me/lib720/caprica/vlcj4/player/embedded/videosurface/callback/UnAllocBufferFormatCallback.java
@@ -21,16 +21,23 @@ package me.lib720.caprica.vlcj4.player.embedded.videosurface.callback;
 
 import me.lib720.caprica.vlcj4.player.embedded.videosurface.CallbackVideoSurface;
 
-import java.nio.ByteBuffer;
-
 /**
  * Callback invoked by the {@link CallbackVideoSurface} when the format of the video changes.
  */
-public interface BufferFormatCallback extends UnAllocBufferFormatCallback {
+public interface UnAllocBufferFormatCallback {
+
     /**
-     * Invoked when new video buffers have been allocated.
+     * Returns a {@link BufferFormat} instance specifying how the {@link CallbackVideoSurface} should structure its
+     * internal buffers.
+     * <p>
+     * Note that it is possible that some versions of VLC provide the wrong value for the sourceHeight parameter, and
+     * more than that it might invoke this callback multiple times with different values for the sourceHeight. Your own
+     * callback implementation may need to mitigate this (e.g. by ignoring the sourceHeight changes on subsequent
+     * invocations of your callback).
      *
-     * @param buffers buffers that were allocated
+     * @param sourceWidth video width
+     * @param sourceHeight video height
+     * @return buffer format, must never be <code>null</code>
      */
-    void allocatedBuffers(ByteBuffer[] buffers);
+    BufferFormat getBufferFormat(int sourceWidth, int sourceHeight);
 }
