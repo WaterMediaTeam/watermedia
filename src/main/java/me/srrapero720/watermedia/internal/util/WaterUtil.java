74b07461b1d18117e840281f859cfe2f3cc21963
SrRapero720 <SrRapero720@hotmail.com>
2023-06-09 17:33:12 -0600

    Updated WaterUtil comments

--- src/main/java/me/srrapero720/watermedia/internal/util/WaterUtil.java
+++ src/main/java/me/srrapero720/watermedia/internal/util/WaterUtil.java
@@ -3,15 +3,23 @@ package me.srrapero720.watermedia.internal.util;
 import java.lang.reflect.Field;
 
 public class WaterUtil {
+    /**
+     * 1 seconds in Minecraft equals 20 ticks
+     * 20x50 equals 1000ms (1 sec)
+     *
+     * @param ticks Minecraft Ticks
+     * @return ticks converted to MS
+     */
+    public static long gameTicksToMs(int ticks) { return ticks * 50L; }
 
-    // 1 segundo equivale a 20 ticks
-    // Multiplicando 20 * 50 obtienes 1000ms lo que equivale a 1 sec
-    public static long gameTicksToMs(int ticks) {
-        return ticks * 50L;
-    }
-    public static int msToGameTicks(long ticks) {
-        return (int) (ticks / 50L);
-    }
+    /**
+     * 1000ms (1 sec) equals 20 ms in Minecraft
+     * 1000/50 equals 20 Ticks (1 sec)
+     *
+     * @param ms Time in milliseconds
+     * @return Milliseconds converted to Ticks
+     */
+    public static int msToGameTicks(long ms) { return (int) (ms / 50L); }
 
     public static <T> Field getClassField(Class<? super T> from, String name) {
         try {
