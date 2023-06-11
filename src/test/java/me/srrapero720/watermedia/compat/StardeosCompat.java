526f7ea839991160cba483157de74ccb08ca8b56
Goedix <goedix+inazucraft@wearehackerone.com>
2023-06-12 00:40:45 +0200

    fix: Updated bins and luac to make work youtube

--- src/test/java/me/srrapero720/watermedia/compat/StardeosCompat.java
+++ src/test/java/me/srrapero720/watermedia/compat/StardeosCompat.java
@@ -3,10 +3,10 @@ package me.srrapero720.watermedia.compat;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.net.*;
 
 public class StardeosCompat {
+
     public static void main(String[] args) {
         String videoId = "63f46ee22ada8aa4fafc11cd"; // ID del video
 
