f99a10df59d721e0d5fb349e8dfe1a4021753e40
SrRapero720 <SrRapero720@hotmail.com>
2023-06-17 08:36:08 -0600

    Test for what?

--- src/test/java/me/srrapero720/watermedia/Testo.java
+++ src/test/java/me/srrapero720/watermedia/Testo.java
@@ -1,12 +1,12 @@
 package me.srrapero720.watermedia;
 
 import me.srrapero720.watermedia.api.video.players.VideoLanPlayer;
-import me.srrapero720.watermedia.api.video.players.events.common.MediaTimeChanged;
+import me.srrapero720.watermedia.api.video.players.events.common.MediaTimeChangedEvent;
 
 public class Testo {
     public static void t() {
         var player = new VideoLanPlayer(null, null, null);
-        player.addEventListener((MediaTimeChanged<VideoLanPlayer>) (player1, oldTime, newTime) -> {
+        player.addEventListener((MediaTimeChangedEvent<VideoLanPlayer>) (player1, oldTime, newTime) -> {
 
         });
 
