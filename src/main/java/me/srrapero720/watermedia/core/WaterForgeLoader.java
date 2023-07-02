93e7d6d777ae1af6e772c7b606e4d4b4f1bdde63
SrRapero720 <SrRapero720@hotmail.com>
2023-07-01 22:46:23 -0600

    Updated crash on DEDICATED_SERVERS

--- src/main/java/me/srrapero720/watermedia/core/WaterForgeLoader.java
+++ src/main/java/me/srrapero720/watermedia/core/WaterForgeLoader.java
@@ -4,6 +4,8 @@ import me.srrapero720.watermedia.WaterMedia;
 import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLDedicatedServerSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLEnvironment;
 import net.minecraftforge.fml.loading.FMLLoader;
 import net.minecraftforge.network.NetworkConstants;
@@ -22,17 +24,21 @@ public class WaterForgeLoader {
 
     public WaterForgeLoader() {
         LOGGER.info(IT, "Running WaterMedia on Forge environment");
-        if (FMLEnvironment.dist.isDedicatedServer()) {
-            LOGGER.error(IT, "###########################  ILLEGAL ENVIRONMENT  ###################################");
-            LOGGER.error(IT, "WATERMeDIA is not designed to run on SERVERS. remove this mod from server to stop crashes");
-            LOGGER.error(IT, "If dependant mods throws error loading WATERMeDIA classes report it to the creator");
-            LOGGER.error(IT, "###########################  ILLEGAL ENVIRONMENT  ###################################");
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::server);
 
-            if (FMLLoader.isProduction()) throw new IllegalStateException("REMOVE WATERMeDIA FROM SERVER_SIDE, THIS IS A CLIENT_SIDE MOD!!!");
-            else LOGGER.warn(IT, "Developer environment detected, ignoring crashes");
-        } else WaterMedia.load();
+        if (!FMLEnvironment.dist.isDedicatedServer()) WaterMedia.load();
 
         //Make sure the mod being absent on the other network side does not cause the client to display the server as incompatible
         ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+    }
+
+    void server(FMLDedicatedServerSetupEvent event) {
+        LOGGER.error(IT, "###########################  ILLEGAL ENVIRONMENT  ###################################");
+        LOGGER.error(IT, "WATERMeDIA is not designed to run on SERVERS. remove this mod from server to stop crashes");
+        LOGGER.error(IT, "If dependant mods throws error loading WATERMeDIA classes report it to the creator");
+        LOGGER.error(IT, "###########################  ILLEGAL ENVIRONMENT  ###################################");
+
+        if (FMLLoader.isProduction()) throw new IllegalStateException("REMOVE WATERMeDIA FROM SERVER_SIDE, THIS IS A CLIENT_SIDE MOD!!!");
+        else LOGGER.warn(IT, "Developer environment detected, ignoring crashes");
     }
 }
