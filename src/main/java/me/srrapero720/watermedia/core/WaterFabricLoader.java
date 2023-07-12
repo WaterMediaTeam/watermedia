acc89d1b599ecf3e519c40ef96a9af88053cdd30
SrRapero720 <SrRapero720@hotmail.com>
2023-07-12 17:19:28 -0600

    Updated fabric loader (try to fix something)

--- src/main/java/me/srrapero720/watermedia/core/WaterFabricLoader.java
+++ src/main/java/me/srrapero720/watermedia/core/WaterFabricLoader.java
@@ -1,7 +1,8 @@
 package me.srrapero720.watermedia.core;
 
 import me.srrapero720.watermedia.WaterMedia;
-import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
 import org.slf4j.Marker;
 import org.slf4j.MarkerFactory;
@@ -13,24 +14,24 @@ import static me.srrapero720.watermedia.WaterMedia.LOGGER;
  * Doing things with FABRIC classes/api is safe
  */
 @Deprecated(forRemoval = true)
-public class WaterFabricLoader implements ClientModInitializer {
+public class WaterFabricLoader implements ModInitializer {
     private static final Marker IT = MarkerFactory.getMarker("FabricLoader");
 
-    public WaterFabricLoader() {
-        LOGGER.info(IT, "Running WaterMedia on Fabric environment");
-
-        LOGGER.error(IT, "###########################  ILLEGAL ENVIRONMENT  ###################################");
-        LOGGER.error(IT, "WATERMeDIA not longer provides support for Fabric environments, breaks basic Java");
-        LOGGER.error(IT, "If you encounter issues loading VLC try install VLC3, if not then report to Fabric");
-        LOGGER.error(IT, "###########################  ILLEGAL ENVIRONMENT  ###################################");
-
-        WaterMedia.init();
-    }
-
     @Override
-    public void onInitializeClient() {
+    public void onInitialize() {
         FabricLoader.getInstance().getAllMods().forEach(modContainer -> {
             if (modContainer.getMetadata().getId().equals("fancyvideo_api")) WaterMedia.onFVADetected();
         });
+
+        LOGGER.info(IT, "Running WaterMedia on Fabric environment");
+
+        if (FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT) {
+            WaterMedia.init();
+        } else {
+            LOGGER.error(IT, "###########################  ILLEGAL ENVIRONMENT  ###################################");
+            LOGGER.error(IT, "WATERMeDIA is not designed to run on SERVERS. remove this mod from server to stop crashes");
+            LOGGER.error(IT, "If dependant mods throws error loading WATERMeDIA classes report it to the creator");
+            LOGGER.error(IT, "###########################  ILLEGAL ENVIRONMENT  ###################################");
+        }
     }
 }
