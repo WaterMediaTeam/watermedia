e0887c6ad37266a36565e7a69ceb2961e2eb6f7f
SrRapero720 <SrRapero720@hotmail.com>
2023-08-03 00:17:58 -0600

    testing kick (works)

--- src/test/java/me/srrapero720/watermedia/compat/Kick.java
+++ src/test/java/me/srrapero720/watermedia/compat/Kick.java
@@ -14,9 +14,10 @@ import static me.srrapero720.watermedia.WaterMedia.LOGGER;
 
 public class Kick extends AbstractFixer {
     public static void main(String[] args) throws MalformedURLException, PatchingUrlException {
-        LOGGER.info(new Kick().patch(new URL("https://kick.com/kenji")));
-        LOGGER.info(new Kick().patch(new URL("https://kick.com/video/af9f91d4-9cbc-4395-8f35-9ef9f245fb8c")));
+//        LOGGER.info(new Kick().patch(new URL("https://kick.com/kenji")));
+//        LOGGER.info(new Kick().patch(new URL("https://kick.com/video/af9f91d4-9cbc-4395-8f35-9ef9f245fb8c")));
     }
+
 
 
     @Override
@@ -25,14 +26,14 @@ public class Kick extends AbstractFixer {
     }
 
     @Override
-    public String patch(URL url) throws PatchingUrlException {
+    public URL patch(URL url) throws PatchingUrlException {
         super.patch(url);
 
         if (url.getPath().contains("/video/")) {
              Call<KickVideo> call = KickAPI.NET.getVideoInfo(url.getPath().replace("/video/", ""));
              try {
                  Response<KickVideo> res = call.execute();
-                 if (res.isSuccessful() && res.body() != null) return res.body().url;
+                 if (res.isSuccessful() && res.body() != null) return new URL(res.body().url);
              } catch (Exception e) {
                  LOGGER.error("Detected Kick.com video but Compat failed to get real URL", e);
              }
@@ -40,7 +41,7 @@ public class Kick extends AbstractFixer {
             Call<KickChannel> call = KickAPI.NET.getChannelInfo(url.getPath().replace("/", ""));
             try {
                 Response<KickChannel> res = call.execute();
-                if (res.isSuccessful() && res.body() != null) return res.body().url;
+                if (res.isSuccessful() && res.body() != null) return new URL(res.body().url);
             } catch (Exception e) {
                 LOGGER.error("Detected Kick.com stream but Compat failed to get real URL", e);
             }
