b6c30523afccf8ee5b2b8907e60211d55833c0a2
SrRapero720 <SrRapero720@hotmail.com>
2023-08-04 09:43:53 -0600

    reinforced crash on startup

--- src/main/java/me/srrapero720/watermedia/WaterMedia.java
+++ src/main/java/me/srrapero720/watermedia/WaterMedia.java
@@ -10,6 +10,7 @@ import org.slf4j.Marker;
 import org.slf4j.MarkerFactory;
 
 import java.io.InputStream;
+import java.util.concurrent.CompletableFuture;
 
 @SuppressWarnings("BooleanMethodIsAlwaysInverted")
 public class WaterMedia {
@@ -22,57 +23,68 @@ public class WaterMedia {
 
 	private final IMediaLoader loader;
 	public WaterMedia(IMediaLoader modLoader) {
-		loader = modLoader;
+		this.loader = modLoader;
 		LOGGER.info(IT, "Running WATERMeDIA on {}", loader.getLoaderName());
 
 		// ENSURE WATERMeDIA IS NOT RUNNING ON SERVERS (except FABRIC)
-		if (!loader.isClient() && !loader.isDev()) {
+		if (!this.loader.isClient() && !this.loader.isDev()) {
+			exception = new IllegalStateException("WATERMeDIA is running on a invalid DIST (dedicated_server)");
+
 			LOGGER.error(IT, "###########################  ILLEGAL ENVIRONMENT  ###################################");
 			LOGGER.error(IT, "WATERMeDIA is not designed to run on SERVERS. remove this mod from server to stop crashes");
 			LOGGER.error(IT, "If dependant mods throws error loading WATERMeDIA classes report it to the creator");
 			LOGGER.error(IT, "###########################  ILLEGAL ENVIRONMENT  ###################################");
-
-			exception = new IllegalStateException("WATERMeDIA is running on a invalid DIST (dedicated_server)");
-		} else LOGGER.info(IT, "Special environment detected, avoiding forced server crash");
+		}
 
 		// ENSURE FANCYVIDEO_API IS NOT INSTALLED (to prevent more bugreports about it)
-		if (loader.isInstalled("fancyvideo_api"))
+		if (this.loader.isInstalled("fancyvideo_api"))
 			exception = new IllegalStateException("FancyVideo-API is explicit incompatible with WATERMeDIA, please remove it");
 
 		// ENSURE IS NOT RUNNING BY TLAUNCHER
-		if (loader.isTLauncher())
+		if (this.loader.isTLauncher())
 			exception = new IllegalStateException("[CRITICAL] TLauncher is a virus launcher and not supported by WATERMeDIA - Suggested: SKLauncher, MultiMC");
 	}
 
 	public void init() {
-		if (!loader.isClient()) return;
+		if (!this.loader.isClient()) return;
 		LOGGER.info(IT, "Starting WaterMedia");
+
+		// RESOURCE EXTRACTOR
+		LOGGER.error(IT, "Loading {}", ResourceSystem.class.getSimpleName());
+		ThreadUtil.trySimple(() -> ResourceSystem.init(this.loader), e -> exception(WaterMediaAPI.class.getSimpleName(), e));
 
 		// PREPARE API
 		LOGGER.info(IT, "Loading {}", WaterMediaAPI.class.getSimpleName());
-		ThreadUtil.trySimple(() -> WaterMediaAPI.init(loader), e -> exception(WaterMediaAPI.class.getSimpleName(), e));
+		ThreadUtil.trySimple(() -> WaterMediaAPI.init(this.loader), e -> exception(WaterMediaAPI.class.getSimpleName(), e));
 
 		// PREPARE STORAGES
 		LOGGER.info(IT, "Loading {}", MediaStorage.class.getSimpleName());
-		ThreadUtil.trySimple(() -> MediaStorage.init(loader), e -> exception(MediaStorage.class.getSimpleName(), e));
+		ThreadUtil.trySimple(() -> MediaStorage.init(this.loader), e -> exception(MediaStorage.class.getSimpleName(), e));
 
 		// PREPARE VLC BINARIES
 		LOGGER.info(IT, "Loading {}", VideoLANBin.class.getSimpleName());
-		ThreadUtil.trySimple(() -> VideoLANBin.init(loader), e -> exception(VideoLANBin.class.getSimpleName(), e));
+		ThreadUtil.trySimple(() -> VideoLANBin.init(this.loader), e -> exception(VideoLANBin.class.getSimpleName(), e));
 
 		// PREPARE VLC
 		LOGGER.info(IT, "Loading {}", VideoLAN.class.getSimpleName());
-		ThreadUtil.trySimple(() -> VideoLAN.init(loader), e -> exception(VideoLAN.class.getSimpleName(), e));
+		ThreadUtil.trySimple(() -> VideoLAN.init(this.loader), e -> exception(VideoLAN.class.getSimpleName(), e));
 
 		// PREPARE LAVAPLAYER
 		LOGGER.info(IT, "Loading {}", LavaPlayer.class.getSimpleName());
-		ThreadUtil.trySimple(() -> LavaPlayer.init(loader), e -> exception(LavaPlayer.class.getSimpleName(), e));
+		ThreadUtil.trySimple(() -> LavaPlayer.init(this.loader), e -> exception(LavaPlayer.class.getSimpleName(), e));
 
 		LOGGER.info(IT, "Finished WaterMedia startup");
-		if (exception != null) LOGGER.warn(IT, "Detected some critical exceptions after startup");
+		if (exception != null) {
+			LOGGER.warn(IT, "Detected some critical exceptions after startup");
+			ThreadUtil.unsafeThread(() -> {
+				ThreadUtil.trySimple(() -> Thread.sleep(60000));
+				if (exception != null) LOGGER.error("WATERMeDIA DETECTS REGISTERED EXCEPTIONS NOT THROW");
+				exceptionThrow();
+			});
+		}
 	}
 
-	public void throwException() { if (exception != null) throw new RuntimeException(exception); }
+	public void exceptionThrow() { if (exception != null) throw new RuntimeException(exception); }
 	private void exception(String module, Exception e) {
 		LOGGER.error(IT, "Exception loading {}", module, e);
 		if (exception != null) exception = e;
