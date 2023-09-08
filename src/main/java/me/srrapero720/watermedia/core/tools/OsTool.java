a89df156414867334826a9e9ce79114b8e4b7a71
SrRapero720 <SrRapero720@hotmail.com>
2023-09-07 21:57:10 -0600

    fixed import

--- src/main/java/me/srrapero720/watermedia/core/tools/OsTool.java
+++ src/main/java/me/srrapero720/watermedia/core/tools/OsTool.java
@@ -1,7 +1,7 @@
 package me.srrapero720.watermedia.core.tools;
 
 import com.sun.jna.Platform;
-import me.lib720.caprica.vlcj.binding.support.runtime.RuntimeUtil;
+import uk.co.caprica.vlcj.binding.RuntimeUtil;
 import org.apache.logging.log4j.Marker;
 import org.apache.logging.log4j.MarkerManager;
 
