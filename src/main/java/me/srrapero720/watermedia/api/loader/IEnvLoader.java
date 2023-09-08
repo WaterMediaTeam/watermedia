541cac6f3dcc480adc34444a57c551b41dd4d502
SrRapero720 <SrRapero720@hotmail.com>
2023-09-07 18:43:03 -0600

    documented bootstrap interface methods

--- src/main/java/me/srrapero720/watermedia/api/loader/IEnvLoader.java
+++ src/main/java/me/srrapero720/watermedia/api/loader/IEnvLoader.java
@@ -1,8 +1,34 @@
 package me.srrapero720.watermedia.api.loader;
 
+/**
+ * This is specially designed for modded environments.
+ * In vanilla Java or... another type of modlauncher is not required
+ */
 public interface IEnvLoader {
+    /**
+     * Checks for the current environments.
+     * @return if was client-side
+     */
     boolean client();
+
+    /**
+     * Checks for any special trigger for development environment.
+     * @return if was a development env
+     */
     boolean development();
+
+    /**
+     * Checks if a mod is installed using the mod id
+     * This can be addressed using FML or FabricLoader
+     * @param modId id of the mod
+     * @return if was installed
+     */
     boolean installed(String modId);
+
+    /**
+     * Aggressive trigger for TLauncher
+     * this was used by internal usages of WATERMeDIA.
+     * @return if the current environment is using TLauncher
+     */
     boolean tlauncher();
 }
--- src/main/java/me/srrapero720/watermedia/api/loader/IMediaLoader.java
+++ src/main/java/me/srrapero720/watermedia/api/loader/IMediaLoader.java
@@ -2,9 +2,37 @@ package me.srrapero720.watermedia.api.loader;
 
 import java.nio.file.Path;
 
+/**
+ * Required for Bootstrap
+ */
 public interface IMediaLoader {
+    /**
+     * Name should be without spaces or number and with capital letters
+     * @return loader name
+     */
     String name();
+
+    /**
+     * Tmp path is normally java.io.tmp directory; you can set up your custom TMP dir,
+     * Just in case you need it (for cleaning or something else)
+     * @return Path instance to tmp directory
+     */
     Path tmpPath();
+
+    /**
+     * Absolute path to current process directory
+     * normally is enough just use `new File("")` but is better do not assume is always it
+     * @return Path instance to process directyr
+     */
     Path processPath();
+
+    /**
+     * Classloader for this module
+     * The main purpose is help to bootstrap find their own JAR resources
+     * @deprecated It was designed to avoid fabric issues getting resources
+     * these issues was fixed already, so is not longer needed a "classloader getter"
+     * @return ClassLoader instance, preferred it was the JAR classloader
+     */
+    @Deprecated
     ClassLoader classLoader();
 }
