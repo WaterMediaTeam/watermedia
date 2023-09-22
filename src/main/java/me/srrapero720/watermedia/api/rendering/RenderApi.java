730de78383ba249982ae317cdb3d023987ffb7b4
SrRapero720 <SrRapero720@hotmail.com>
2023-09-22 05:33:23 -0600

    base class for RenderAPI

---
+++ src/main/java/me/srrapero720/watermedia/api/rendering/RenderApi.java
@@ -0,0 +1,8 @@
+package me.srrapero720.watermedia.api.rendering;
+
+/**
+ * RenderApi is a tool class for OpenGL rendering compatible with all minecraft versions
+ */
+public class RenderApi {
+
+}
---
+++ src/main/java/me/srrapero720/watermedia/api/rendering/RenderGL.java
@@ -0,0 +1,4 @@
+package me.srrapero720.watermedia.api.rendering;
+
+public class RenderGL {
+}
