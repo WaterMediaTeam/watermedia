e74b08314073823c6bcc569c0c56f2300415d175
SrRapero720 <SrRapero720@hotmail.com>
2023-09-30 23:39:19 -0600

    fixed log4j doesn't log on tests

--- src/test/java/me/srrapero720/watermedia/FixerTester.java
+++ src/test/java/me/srrapero720/watermedia/FixerTester.java
@@ -5,21 +5,24 @@ import me.srrapero720.watermedia.api.url.fixers.URLFixer;
 
 public class FixerTester {
     public static void main(String[] args) {
+
+        // Set up a simple configuration that logs on the console.
+
         try {
             UrlAPI.init(null);
         } catch (Exception e) {}
 
         URLFixer.Result result = UrlAPI.fixURL("https://imgur.com/t/rick_roll/kGy6J8J");
-        System.out.println("Result is: " + result);
+        WaterMedia.LOGGER.info("Result is: {}", result);
 
         result = UrlAPI.fixURL("https://imgur.com/4JMVauZ");
-        System.out.println("Result is: " + result);
+        WaterMedia.LOGGER.info("Result is: {}", result);
 
         result = UrlAPI.fixURL("https://imgur.com/a/TIiiR21");
-        System.out.println("Result is: " + result);
+        WaterMedia.LOGGER.info("Result is: {}", result);
 
         result = UrlAPI.fixURL("https://imgur.com/t/art/gm4Q7kN");
-        System.out.println("Result is: " + result);
+        WaterMedia.LOGGER.info("Result is: {}", result);
 
         System.exit(0);
     }
