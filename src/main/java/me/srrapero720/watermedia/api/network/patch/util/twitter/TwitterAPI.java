a78268d954c5ac633b962e57ab00f0484bfb8111
NGoedix <goedix@wearehackerone.com>
2024-04-14 21:16:07 +0200

    Fix: X (Formerly Twitter) patch

--- src/main/java/me/srrapero720/watermedia/api/network/patch/util/twitter/TwitterAPI.java
+++ src/main/java/me/srrapero720/watermedia/api/network/patch/util/twitter/TwitterAPI.java
@@ -31,6 +31,7 @@ public class TwitterAPI {
         URL url = new URL(tweetUrl);
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
+        conn.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:124.0) Gecko/20100101 Firefox/124.0");
         String response = readResponse(conn);
 
         // Find main.js URL
