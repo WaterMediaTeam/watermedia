b83bff9ee5c40a93e60c60641d08053f2b186f0b
J-RAP <SrRapero720@hotmail.com>
2024-04-26 22:46:24 -0600

    backported MathAPI

--- src/main/java/me/srrapero720/watermedia/api/math/MathAPI.java
+++ src/main/java/me/srrapero720/watermedia/api/math/MathAPI.java
@@ -14,6 +14,7 @@ public class MathAPI extends WaterMediaAPI {
      * @deprecated <p>Tick type was changed from int to long following the time counting standard.</p>
      * Use instead {@link MathAPI#tickToMs(long)}
      */
+    @Deprecated
     public static long tickToMs(int ticks) { return ticks * 50L; }
 
     /**
@@ -44,48 +45,112 @@ public class MathAPI extends WaterMediaAPI {
     public static int msToTick(long ms) { return (int) (ms / 50); }
 
     /**
-     * Missing docs
-     * @param start
-     * @param end
-     * @param time
-     * @return
+     * Returns a precise scale of a start time and end time.
+     * If time is out of range, it will be reset to zero
+     * ej start: 0, end: 10, time: 15 - Result will be 0.5d
+     * @param start initial point to calculate scale, cannot be negative
+     * @param end final point to calculate scale. cannot be negative
+     * @param time current time between values
+     * @return scaled time
      */
     public static double scaleTempo(long start, long end, long time) {
         if (start < 0 || end < 0 || time < 0) throw new IllegalArgumentException("Invalid negative value");
-        try {
-            long duration = end - start; // start acts like a margin
-            long realTime = time - start;
 
-            long result = realTime / duration;
-            if (realTime > duration) result %= end - start;
-            return result;
-        } catch (ArithmeticException ignored) {
-            return 0;
-        }
-    }
+        long duration = end - start; // start acts like a margin
+        long realTime = time - start;
 
-    public static double scaleTempo(double start, double end, double time) {
-        if (start < 0 || end < 0 || time < 0) throw new IllegalArgumentException("Invalid negative value");
-        try {
-            double duration = end - start; // start acts like a margin
-            double realTime = time - start;
+        if (realTime == 0 || duration == 0) return 0; // not ArithmeticException
 
-            double result = realTime / duration;
-            if (realTime > duration) result %= end - start;
-            return result;
-        } catch (ArithmeticException ignored) {
-            return 0;
-        }
+        long result = realTime / duration;
+        if (realTime > duration) result %= duration;
+        return result;
     }
 
     /**
-     * missing docs
-     * @param startTick
-     * @param endTick
-     * @param timeTick
-     * @return
+     * Returns a precise scale of a start time and end time.
+     * If time is out of range, it will be reset to zero
+     * ej start: 0, end: 10, time: 15 - Result will be 0.5d
+     * @param start initial point to calculate scale, cannot be negative
+     * @param end final point to calculate scale. cannot be negative
+     * @param time current time between values
+     * @return scaled time
+     */
+    public static double scaleTempo(double start, double end, double time) {
+        if (start < 0 || end < 0 || time < 0) throw new IllegalArgumentException("Invalid negative value");
+        double duration = end - start; // start acts like a margin
+        double realTime = time - start;
+
+        if (realTime == 0 || duration == 0) return 0; // not ArithmeticException
+
+        double result = realTime / duration;
+        if (realTime > duration) result %= duration;
+        return result;
+    }
+
+    /**
+     * Returns a precise scale of a start time and end time.
+     * If time is out of range, it will be reset to zero
+     * ej start: 0, end: 10, time: 15 - Result will be 0.5d
+     * Method helper to calculate time in ticks (20t = 1s)
+     * @param startTick initial point to calculate scale, cannot be negative
+     * @param endTick final point to calculate scale. cannot be negative
+     * @param timeTick current time between values
+     * @return scaled time
      */
     public static double scaleTempoTick(int startTick, int endTick, int timeTick) {
+        return scaleTempo(MathAPI.tickToMs(startTick), MathAPI.tickToMs(endTick), MathAPI.tickToMs(timeTick));
+    }
+
+    /**
+     * Returns a precise scale of a start time and end time.
+     * If time is out of range, it will be over-scaled
+     * ej start: 0, end: 10, time: 15 - Result will be 1.5d
+     * @param start initial point to calculate scale, cannot be negative
+     * @param end final point to calculate scale. cannot be negative
+     * @param time current time between values
+     * @return scaled time
+     */
+    public static double scaleDesTempo(long start, long end, long time) {
+        if (start < 0 || end < 0 || time < 0) throw new IllegalArgumentException("Invalid negative value");
+        long duration = end - start; // start acts like a margin
+        long realTime = time - start;
+
+        if (realTime == 0 || duration == 0) return 0; // not ArithmeticException
+
+        return (double) realTime / duration;
+    }
+
+    /**
+     * Returns a precise scale of a start time and end time.
+     * If time is out of range, it will be over-scaled
+     * ej start: 0, end: 10, time: 15 - Result will be 1.5d
+     * @param start initial point to calculate scale, cannot be negative
+     * @param end final point to calculate scale. cannot be negative
+     * @param time current time between values
+     * @return scaled time
+     */
+    public static double scaleDesTempo(double start, double end, double time) {
+        if (start < 0 || end < 0 || time < 0) throw new IllegalArgumentException("Invalid negative value");
+        double duration = end - start; // start acts like a margin
+        double realTime = time - start;
+
+        if (realTime == 0 || duration == 0) return 0; // not ArithmeticException
+
+        double result = realTime / duration;
+        return (Double.isNaN(result)) ? 0 : result;
+    }
+
+    /**
+     * Returns a precise scale of a start time and end time.
+     * If time is out of range, it will be over-scaled
+     * ej start: 0, end: 10, time: 15 - Result will be 1.5d
+     * Method helper to calculate time in ticks (20t = 1s)
+     * @param startTick initial point to calculate scale, cannot be negative
+     * @param endTick final point to calculate scale. cannot be negative
+     * @param timeTick current time between values
+     * @return scaled time
+     */
+    public static double scaleDesTempoTick(int startTick, int endTick, int timeTick) {
         return scaleTempo(MathAPI.tickToMs(startTick), MathAPI.tickToMs(endTick), MathAPI.tickToMs(timeTick));
     }
 
@@ -112,34 +177,43 @@ public class MathAPI extends WaterMediaAPI {
      * @since 2.0.7
      */
     public static long floorMod(long x, long y) {
-        try {
-            final long r = x % y;
-            // if the signs are different and modulo not zero, adjust result
-            if ((x ^ y) < 0 && r != 0) {
-                return r + y;
-            }
-            return r;
-        } catch (ArithmeticException e) {
-            return 0;
+        if (x == 0 || y == 0) return 0;
+
+        final long r = x % y;
+        // if the signs are different and modulo not zero, adjust result
+        if ((x ^ y) < 0 && r != 0) {
+            return r + y;
         }
+        return r;
     }
 
     public static int floorMod(int x, int y) {
-        try {
-            final int r = x % y;
-            // if the signs are different and modulo not zero, adjust result
-            if ((x ^ y) < 0 && r != 0) {
-                return r + y;
-            }
-            return r;
-        } catch (ArithmeticException e) {
-            return 0;
+        if (x == 0 || y == 0) return 0;
+
+        final int r = x % y;
+        // if the signs are different and modulo not zero, adjust result
+        if ((x ^ y) < 0 && r != 0) {
+            return r + y;
         }
+        return r;
     }
 
     public static int floorMod(long x, int y) {
         // Result cannot overflow the range of int.
         return (int) floorMod(x, (long)y);
+    }
+
+    public static int floorMod(int x, long y) {
+        // Result cannot overflow the range of int.
+        return (int) floorMod((long) x, y);
+    }
+
+    public static short min(short a, short b) {
+        return (a <= b) ? a : b;
+    }
+
+    public static byte min(byte a, byte b) {
+        return (a <= b) ? a : b;
     }
 
     /**
@@ -151,6 +225,18 @@ public class MathAPI extends WaterMediaAPI {
      * @param b Blue
      * @return HEX color
      */
+    public static int argb(int a, int r, int g, int b) { return (a << 24) | (r << 16) | (g << 8) | b; }
+
+    /**
+     * Creates a hexadecimal color based on gave params
+     * All values need to be in a range of 0 ~ 255
+     * @param a Alpha
+     * @param r Red
+     * @param g Green
+     * @param b Blue
+     * @return HEX color
+     * @deprecated renamed to {@link MathAPI#argb(int, int, int, int)}
+     */
     public static int getColorARGB(int a, int r, int g, int b) { return (a << 24) | (r << 16) | (g << 8) | b; }
 
     /**
@@ -159,6 +245,9 @@ public class MathAPI extends WaterMediaAPI {
      * @param end end of the result across time
      * @param t time from 0.0 ~ 1.0
      * @return calculated result of ease-in
+     *
+     * @see MathAPI#scaleTempo(long, long, long)
+     * @see MathAPI#scaleDesTempo(long, long, long)
      */
     public static double easeIn(double start, double end, double t) {
         return start + (end - start) * t * t;
@@ -170,6 +259,9 @@ public class MathAPI extends WaterMediaAPI {
      * @param end end of the result across time
      * @param t time from 0.0 ~ 1.0
      * @return calculate result of ease-out
+     *
+     * @see MathAPI#scaleTempo(long, long, long)
+     * @see MathAPI#scaleDesTempo(long, long, long)
      */
     public static double easeOut(double start, double end, double t) {
         return start + (end - start) * (1 - Math.pow(1 - t, 2));
@@ -181,6 +273,9 @@ public class MathAPI extends WaterMediaAPI {
      * @param end end of the result across time
      * @param t time from 0.0 ~ 1.0
      * @return calculate result of ease-in-out
+     *
+     * @see MathAPI#scaleTempo(long, long, long)
+     * @see MathAPI#scaleDesTempo(long, long, long)
      */
     public static double easeInOut(double start, double end, double t) {
         return t < 0.5 ? easeIn(start, end / 2, t * 2) : easeOut(start + (end / 2), end, (t - 0.5) * 2);
@@ -192,6 +287,9 @@ public class MathAPI extends WaterMediaAPI {
      * @param end end of the result across time
      * @param t time from 0.0 ~ 1.0
      * @return calculate result of ease-out-in
+     *
+     * @see MathAPI#scaleTempo(long, long, long)
+     * @see MathAPI#scaleDesTempo(long, long, long)
      */
     public static double easeOutIn(double start, double end, double t) {
         return t < 0.5 ? easeOut(start, end / 2, t * 2) : easeIn(start + (end / 2), end, (t - 0.5) * 2);
@@ -203,6 +301,9 @@ public class MathAPI extends WaterMediaAPI {
      * @param end end of the result across time
      * @param t time from 0.0 ~ 1.0
      * @return calculate result of ease-in-circle
+     *
+     * @see MathAPI#scaleTempo(long, long, long)
+     * @see MathAPI#scaleDesTempo(long, long, long)
      */
     public static double easeInCircle(double start, double end, double t) {
         return start + (end - start) * (1 - Math.sqrt(1 - t * t));
@@ -214,6 +315,9 @@ public class MathAPI extends WaterMediaAPI {
      * @param end end of the result across time
      * @param t time from 0.0 ~ 1.0
      * @return calculate result of ease-out-circle
+     *
+     * @see MathAPI#scaleTempo(long, long, long)
+     * @see MathAPI#scaleDesTempo(long, long, long)
      */
     public static double easeOutCircle(double start, double end, double t) {
         return start + (end - start) * Math.sqrt(1 - Math.pow(t - 1, 2));
@@ -225,6 +329,9 @@ public class MathAPI extends WaterMediaAPI {
      * @param end end of the result across time
      * @param t time from 0.0 ~ 1.0
      * @return calculate result of easy-ease
+     *
+     * @see MathAPI#scaleTempo(long, long, long)
+     * @see MathAPI#scaleDesTempo(long, long, long)
      */
     public static double easyEase(double start, double end, double t) {
         return start + (end - start) * ((t < 0.5) ? 2 * t * t : -1 + 2 * t * (2 - t));
