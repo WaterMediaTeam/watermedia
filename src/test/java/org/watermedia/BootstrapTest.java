cf372eef6465eef1acd4c612a36ce4697dae1e6c
J-RAP <SrRapero720@hotmail.com>
2024-10-05 22:04:37 -0600

    well minor changes

--- src/test/java/org/watermedia/BootstrapTest.java
+++ src/test/java/org/watermedia/BootstrapTest.java
@@ -1,13 +1,11 @@
 package org.watermedia;
 
 import me.srrapero720.watermedia.WaterMedia;
-import me.srrapero720.watermedia.api.MediaContext;
 import me.srrapero720.watermedia.loader.ILoader;
 import org.junit.jupiter.api.BeforeAll;
 
 
 public class BootstrapTest {
-    public static final MediaContext CONTEXT = new MediaContext.Simple("test", "Test");
 
     @BeforeAll
     public static void testBootstrap() throws Exception {
--- src/test/java/org/watermedia/network/TwitterPatchTest.java
+++ src/test/java/org/watermedia/network/TwitterPatchTest.java
@@ -1,24 +1,25 @@
 package org.watermedia.network;
 
+import me.srrapero720.watermedia.api.MediaContext;
 import me.srrapero720.watermedia.api.network.MediaURI;
 import me.srrapero720.watermedia.api.network.patchs.TwitterPatch;
 import org.junit.jupiter.api.Test;
-import org.watermedia.BootstrapTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static me.srrapero720.watermedia.WaterMedia.*;
 
 public class TwitterPatchTest {
+    public static final MediaContext CONTEXT = new MediaContext.Simple("twitter_test", "Twitter Patch test");
     private static final String EXAMPLE_URI = "https://x.com/SrRap720/status/1842440861964984539";
 
     @Test
     public void testValidatorData() {
-        MediaURI source = MediaURI.get(BootstrapTest.CONTEXT, EXAMPLE_URI);
+        MediaURI source = MediaURI.get(CONTEXT, EXAMPLE_URI);
         TwitterPatch patch = new TwitterPatch();
 
         assertTrue(patch.validate(source));
         try {
-            patch.patch(source, BootstrapTest.CONTEXT);
+            patch.patch(source, CONTEXT);
         } catch (Exception e) {
             throw new RuntimeException("Failed to patch URL", e);
         }
@@ -26,8 +27,6 @@ public class TwitterPatchTest {
         assertTrue(source.patched());
         var sources = source.getSources();
         assertEquals(4, sources.length);
-
-        LOGGER.debug(sources.length);
 
         for (MediaURI.Source s: sources) {
             LOGGER.debug(s.toString());
