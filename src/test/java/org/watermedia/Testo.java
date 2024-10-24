c1c06b5919ed2d45bd1d6b8ce6dc75743721da58
J-RAP <SrRapero720@hotmail.com>
2024-10-24 10:32:15 -0600

    ridiculous

--- src/test/java/org/watermedia/Testo.java
+++ src/test/java/org/watermedia/Testo.java
@@ -1,8 +1,13 @@
 package org.watermedia;
 
+import java.net.URI;
+
 public class Testo {
     public static void main(String[] args) {
+        WaterMedia.LOGGER.info("ne");
 
+        URI u = URI.create("water://local.wm/");
+        WaterMedia.LOGGER.info("AUTHORITY: {} || HOST: {} || PROTOCOL: {}", u.getAuthority(), u.getHost(), u.getScheme());
 
 //        String[] arr1 = new String[] { "value-1", "value-2", "value-3" };
 //        String[] arr2 = new String[] { "extra-1", "extra-2", "extra-3" };
