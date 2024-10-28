5c73fe68251ef6e6ae9d8c98f272204f1172e976
J-RAP <SrRapero720@hotmail.com>
2024-10-27 19:46:07 -0600

    yet on servers?

--- src/main/java/org/watermedia/core/exceptions/IllegalEnvironmentException.java
+++ src/main/java/org/watermedia/core/exceptions/IllegalEnvironmentException.java
@@ -7,7 +7,7 @@ import static org.watermedia.WaterMedia.LOGGER;
 
 public class IllegalEnvironmentException extends Exception {
     public IllegalEnvironmentException() {
-        super(WaterMedia.NAME + " CANNOT be installed on SERVER-SIDE. Please remove " + WaterMedia.NAME + " from the server, and keep it on client");
+        super(WaterMedia.NAME + " CANNOT be installed YET on SERVER-SIDE. Please remove " + WaterMedia.NAME + " from the server, and keep it on client");
         LOGGER.fatal(IT, "##############################  ILLEGAL ENVIRONMENT  ######################################");
         LOGGER.fatal(IT, "{} is not designed to work on server-side, please remove it from server and keep it on client", WaterMedia.NAME);
         LOGGER.fatal(IT, "Dependent mods can work without {} ON SERVERS, remember keep the mod ONLY ON CLIENT-SIDE", WaterMedia.NAME);
