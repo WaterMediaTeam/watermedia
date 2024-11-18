a1fe51ee4a5bfd9f9e4c2e7126ee05e7bfd708a2
J-RAP <SrRapero720@hotmail.com>
2024-11-18 14:11:08 -0600

    eeeeeeee

--- src/main/java/org/watermedia/core/network/NetworkRequest.java
+++ src/main/java/org/watermedia/core/network/NetworkRequest.java
@@ -1,7 +1,35 @@
 package org.watermedia.core.network;
 
-import java.net.http.HttpClient;
+import org.apache.logging.log4j.Marker;
+import org.apache.logging.log4j.MarkerManager;
+import org.watermedia.tools.ThreadTool;
 
-public class NetworkRequest {
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class NetworkRequest implements Runnable {
+    private static final Marker IT = MarkerManager.getMarker("ImageAPI");
+    private static final DateFormat FORMAT = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss z");
+    private static final ExecutorService EXECUTOR = Executors.newScheduledThreadPool(ThreadTool.minThreads(), ThreadTool.factory("ImageFetch-Worker", Thread.NORM_PRIORITY + 1));
+
+
+    public NetworkRequest() {
+
+    }
+
+
+    public void start() {
+        EXECUTOR.execute(this);
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+
+
 
 }
